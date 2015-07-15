2b332de618abb24d306b839dcab7fa1f08aaf763
topgrd <topgrd@gmail.com>
2015-07-15 16:57:51 +0800

    word wrap

--- slide.js
+++ slide.js
@@ -4,8 +4,8 @@
  * Date: 2015-05-18
  * Time: 16:00
  */
-(function ($) {
-	$.fn.slide = function (options) {
+(function($) {
+	$.fn.slide = function(options) {
 		/**
 		 * 默认设置
 		 * start: true or false 默认开始
@@ -18,7 +18,7 @@
 			animate: "horizontal"
 		}
 		opts = $.extend({}, this.defaults, options);
-		this.each(function () {
+		this.each(function() {
 			var slideContainer = $(this).find('.slide-container');
 			var animate = opts.animate;
 			var slide = slideContainer.find('li');
@@ -26,106 +26,108 @@
 			var index = 0;
 			var time = null;
 			var _this = this;
-			var speed = opts.speed||2000;
-			$(this).data('opts',opts);
+			var speed = opts.speed || 2000;
+			$(this).data('opts', opts);
 			$(slide[0]).show();
 			//向页面增加前进后退按钮元素
-			var ctrlHtml 
-			= '<a href="javascript:;" class="ctrl-slide prev"></a>'
-			+ '<a href="javascript:;" class="ctrl-slide next"></a>';
+			var ctrlHtml = '<a href="javascript:;" class="ctrl-slide prev"></a>' + '<a href="javascript:;" class="ctrl-slide next"></a>';
 			$('.slide').append(ctrlHtml);
-			var pageIndex 
-			= '<ul class="slide-tabs">';
-			for (var i=0; i<count;i++) {
+			var pageIndex = '<ul class="slide-tabs">';
+			for (var i = 0; i < count; i++) {
 				pageIndex += '<li><a href="#" class="slide-a"></a></li>'
 			}
 			//向页面增加trigger
 			pageIndex += '</ul>';
 			$('.slide').append(pageIndex);
+
 			function start() {
 				if (opts.start) {
-					time = setInterval(function () {
+					time = setInterval(function() {
 						var old = index;
 						if (index >= count - 1) {
 							index = 0;
 						}
 						else {
 							index++;
-					    }
+						}
 						change.call(_this, index, old);
 					}, speed);
 				}
 			};
 
-			$(this).find('.next').on('click', function () {
+			$(this).find('.next').on('click', function() {
 				var old = index;
-						if (index >= count - 1) {
-							index = 0;
-						}
-						else {
-							index++;
-						}
+				if (index >= count - 1) {
+					index = 0;
+				}
+				else {
+					index++;
+				}
 				change.call(_this, index, old);
 
 			});
 
-			$(this).find('.prev').on('click', function () {
-				var old = index;
-						if (index <= 0) {
-							index = count - 1;
-						}
-						else {
-							index--;
-						}
+			$(this).find('.prev').on('click', function() {
+			    var old = index;
+				if (index <= 0) {
+					index = count - 1;
+				}
+				else {
+					index--;
+				}
 				change.call(_this, index, old, 'left');
 
 			});
 			//鼠标划入暂停
-			$(this).on('mouseover', function () {
+			$(this).on('mouseover', function() {
 				if (opts.start) {
 					clearInterval(time);
 				}
-				$(this).find('.ctrl-slide').css({opacity:0.6});
+				$(this).find('.ctrl-slide').css({
+					opacity: 0.6
+				});
 			})
 
-			$(this).on('mouseout', function () {
+			$(this).on('mouseout', function() {
 				if (opts.start) {
 					start();
 				}
-				$(this).find('.ctrl-slide').css({opacity:0});
+				$(this).find('.ctrl-slide').css({
+					opacity: 0
+				});
 			})
 
 			//trigger
-			$(this).find('.slide-tabs li').each(function (aIndex) {
-				$(this).on('click.slide-tabs', function () {
-					if (aIndex<index) {
+			$(this).find('.slide-tabs li').each(function(aIndex) {
+				$(this).on('click.slide-tabs', function() {
+					if (aIndex < index) {
 						change.call(_this, aIndex, index, 'left');
 					}
 					else if (aIndex == index) {
-						
+
 					}
 					else {
 						change.call(_this, aIndex, index);
 					}
-					
+
 					index = aIndex;
 				});
 			});
 
-			switch(opts.animate){
-                case "horizontal":
-                    opts['width'] = $(this).width();
-                    slide.css('left', -opts['width']);
-                    $(slide[0]).css('left', 0);
-                    slide.show();
-                    break;
-                case "opacity":
-                	 slideContainer.css({
-                	 	'position': 'relative'
-                	 });
-                	 slide.css('display', 'none');
-                	 $(slide[0]).show();   
-            }
+			switch (opts.animate) {
+				case "horizontal":
+					opts['width'] = $(this).width();
+					slide.css('left', -opts['width']);
+					$(slide[0]).css('left', 0);
+					slide.show();
+					break;
+				case "opacity":
+					slideContainer.css({
+						'position': 'relative'
+					});
+					slide.css('display', 'none');
+					$(slide[0]).show();
+			}
 
 			start();
 		});
@@ -140,26 +142,48 @@
 			var y = hideIndex % (slide.length);
 			var slideWidth = opts['width'];
 			if (left == 'left') {
-				slide.eq(x).stop().css("left", -slideWidth).addClass("active").animate({left: 0});
-				slide.eq(y).stop().css("left", 0).animate({left: slideWidth}, function (){
+				slide.eq(x).stop().css("left", -slideWidth).addClass("active").animate({
+					left: 0
+				});
+				slide.eq(y).stop().css("left", 0).animate({
+					left: slideWidth
+				}, function() {
 					slide.eq(x).removeClass('active');
 				});
 			}
 			else {
-				slide.eq(x).stop().css("left", slideWidth).addClass("active").animate({left: 0});
-				slide.eq(y).stop().css("left", 0).animate({left: -slideWidth}, function (){
+				slide.eq(x).stop().css("left", slideWidth).addClass("active").animate({
+					left: 0
+				});
+				slide.eq(y).stop().css("left", 0).animate({
+					left: -slideWidth
+				}, function() {
 					slide.eq(x).removeClass('active');
 				});
 			}
-			
-			$(this).find('.slide-tabs li').eq(hideIndex).css({opacity: 0.7});
-			$(this).find('.slide-tabs li').eq(showIndex).css({opacity: 1});
+
+			$(this).find('.slide-tabs li').eq(hideIndex).css({
+				opacity: 0.7
+			});
+			$(this).find('.slide-tabs li').eq(showIndex).css({
+				opacity: 1
+			});
 		}
 		else {
-			$(this).find('.slide-container li').eq(hideIndex).stop().hide().animate({opacity: 0});
-			$(this).find('.slide-tabs li').eq(hideIndex).css({opacity: 0.7});
-			$(this).find('.slide-tabs li').eq(showIndex).css({opacity: 1});
-			$(this).find('.slide-container li').eq(showIndex).show().css({opacity: 0}).stop().animate({opacity: 1});
+			$(this).find('.slide-container li').eq(hideIndex).stop().hide().animate({
+				opacity: 0
+			});
+			$(this).find('.slide-tabs li').eq(hideIndex).css({
+				opacity: 0.7
+			});
+			$(this).find('.slide-tabs li').eq(showIndex).css({
+				opacity: 1
+			});
+			$(this).find('.slide-container li').eq(showIndex).show().css({
+				opacity: 0
+			}).stop().animate({
+				opacity: 1
+			});
 		}
 	};
 })(jQuery);
